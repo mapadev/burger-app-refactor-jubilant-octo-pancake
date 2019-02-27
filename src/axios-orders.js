5a501faae175177df429fe64b45189e2941d5ce1
mapadev <m4ttek@gmail.com>
2019-02-27 19:41:32 +0100

    refactored burgerBuilder and order actions to use sagas

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'https://react-burger.firebaseio.com/'
     baseURL: "https://udemy-react-my-burger.firebaseapp.com/"
 });
 
