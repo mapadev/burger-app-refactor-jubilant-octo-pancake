8978d0d74e3f74149995328bb62f9fd0bda93737
mapadev <m4ttek@gmail.com>
2019-02-27 17:18:33 +0100

    refactored checkState from thunk to saga

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -49,3 +49,23 @@ export function* authUserSaga(action) {
         yield put(actions.authFail(err.response.data.error));
     }
 }
+
+export function* authCheckStateSaga() {
+    const token = localStorage.getItem("token");
+    if (!token) {
+        yield put(actions.logout());
+    } else {
+        const expirationDate = new Date(localStorage.getItem("expirationDate"));
+        if (expirationDate <= new Date()) {
+            yield put(actions.logout());
+        } else {
+            const userId = localStorage.getItem("userId");
+            yield put(actions.authSuccess(token, userId));
+            yield put(
+                actions.checkAuthTimeout(
+                    (expirationDate.getTime() - new Date().getTime()) / 1000
+                )
+            );
+        }
+    }
+}
